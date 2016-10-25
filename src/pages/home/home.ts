51f0feeced62e811a557795a7d60b5bf28c94ffb
ebbevang <ev@evang.dk>
2016-10-25 12:32:01 +0200

    adds animation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,14 @@ import {Input, trigger, state, style, transition, animate} from '@angular/core';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
   animations: [
-
+    trigger('visibilityChanged', [
+      state("shown" , style({ opacity: 1, marginLeft: 0 })), 
+      state("hidden", style({ opacity: 0, marginLeft: 20 })),
+      transition('shown => hidden', animate('600ms')),
+      transition('hidden => shown', animate('300ms')),
+    ])
   ]
 })
 export class HomePage {
@@ -16,10 +21,22 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
     
   }
-  //http://blog.thoughtram.io/angular/2016/09/16/angular-2-animation-important-concepts.html
-  visibility = 'shown';
+  
+  visibility : string = 'shown';
+  
+  //@Input() animatedVisibility : boolean = true;
+  @Input() animatedVisibility : string = "shown";
 
   btnClick() {
-    this.visibility == "shown" ? this.visibility= "hidden" : this.visibility = "shown";
+        this.visibility == "shown" ? this.visibility= "hidden" : this.visibility = "shown";
+        console.log("visibility is now "+this.visibility);
+  }
+
+  btnClickAnimated() {
+    this.animatedVisibility == "shown" ? this.animatedVisibility= "hidden" : this.animatedVisibility = "shown";
+    console.log("animatedVisibility is now "+this.animatedVisibility);
   }
 }
+
+// a very good explanation of the new animation in angular2
+//http://blog.thoughtram.io/angular/2016/09/16/angular-2-animation-important-concepts.html
